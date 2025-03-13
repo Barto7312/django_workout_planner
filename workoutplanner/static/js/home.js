e7a0520ab020d32565cbb6a7e85b3b5fa4faabed
Bartosz Piwowarczyk <Bartosz Piwowarczyk@issp.wro>
2025-03-13 16:00:09 +0100

    home WIP

--- workoutplanner/static/js/home.js
+++ workoutplanner/static/js/home.js
@@ -13,6 +13,7 @@ function getWorkout(workoutId){
             return;
         }
 
+        for exercise in data.exercises_data:
 
         document.getElementById("workoutTitle").innerHTML = data.workout_plan.name;
 
@@ -24,16 +25,30 @@ function getWorkout(workoutId){
 
     function createUi(){
         const mainBox = document.getElementById("exercisesBox");
-    
+
+        //exercise div
         const exerciseDiv = document.createElement("div");
         mainBox.appendChild(exerciseDiv);
 
+        //info div
         const infoDiv = document.createElement("div");
         infoDiv.classList.add("display-info");
         exerciseDiv.appendChild(infoDiv);
+
+        const setsRepsDiv = document.createElement("div");
+        setsRepsDiv.classList.add("display-sets-reps");
+        infoDiv.appendChild(setsRepsDiv);
+
+        const weightDiv = document.createElement("div");
+        weightDiv.classList.add("display-weight");
+        infoDiv.appendChild(weightDiv);
+
+        //name div
         const nameDiv = document.createElement("div");
         infoDiv.classList.add("display-name");
         exerciseDiv.appendChild(nameDiv);
+
+        //rest div
         const restDiv = document.createElement("div");
         infoDiv.classList.add("display-rest");
         exerciseDiv.appendChild(restDiv);
