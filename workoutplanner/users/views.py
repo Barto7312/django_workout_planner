befb50f5ab5db6bb7652848231ad5833c1d7b106
Barto7312 <ksgie@wp.pl>
2025-03-24 22:01:17 +0100

    Register page WIP

--- workoutplanner/users/views.py
+++ workoutplanner/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.conf import settings
@@ -16,17 +16,13 @@ def loginUser(request):
         username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
-        try:
-            user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'User does not exist')
-
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('/')
         else:
-            messages.error(request, "Username OR password doesn't exist")
+            messages.error(request, "Invalid username or password")
         
         return redirect('login')
 
@@ -35,26 +31,22 @@ def loginUser(request):
     return render(request, 'login_register.html', context)
 
 def registerUser(request):
-    
     if request.user.is_authenticated:
         return redirect('/')      
 
-    form = UserCreationForm()
-
+    form = CustomUserCreationForm() 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
             login(request, user)
-            return redirect ('/')
+            return redirect('/')
         else:
-            messages.error(request, "Error occured during registration")
+            messages.error(request, "Error occurred during registration")
 
-            return redirect('register')
-
-    context = {'form': form,'hide_nav': True}
+    context = {'form': form, 'hide_nav': True}
     return render(request, 'login_register.html', context)
 
 def logoutUser(request):
